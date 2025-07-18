afe795e3efff2b08ceec572cc7ae87df8e3e7df9
mfsohail12 <149211499+mfsohail12@users.noreply.github.com>
2025-07-17 22:27:59 -0400

    added footer to homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import "./App.css";
+import { FaGithub } from "react-icons/fa";
 
 axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
 
@@ -11,6 +12,12 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <footer className="h-10 flex justify-center items-center absolute bottom-0 w-screen px-4">
+        <a href="https://github.com/mfsohail12/FileCompressor">
+          <FaGithub className="text-red-400 text-xl" />
+        </a>
+        <p className="text-white text-xs ml-auto">© 2025</p>
+      </footer>
     </>
   );
 }
